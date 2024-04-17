ea78330c53bc3c638ae5add9dec1cc7f1031b3f4
Dawood Rehman <109825112+dawoodrehman44@users.noreply.github.com>
2024-04-18 02:05:27 +0500

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -9,4 +9,24 @@ this_dic = {
 }
 print(this_dic)
 
+##############################################################################################
+# Creating a dictionary
+student = {
+    "name": "John",
+    "age": 20,
+    "major": "Computer Science"
+}
+
+# Accessing values in the dictionary
+print(student["name"])  # Output: John
+print(student["age"])   # Output: 20
+
+# Modifying a dictionary
+student["age"] = 21  # Modify value
+student["year"] = 3  # Add new key-value pair
+del student["major"] # Remove a key-value pair
+
+# Printing the modified dictionary
+print(student)
+##############################################################################################
 
